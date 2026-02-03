ba45361163dd88ef2ca321e9b61440fd8274a74f
mohana-pamidi <mohanashree.pamidimukkala@gmail.com>
2026-02-03 15:51:10 -0500

    added skeleton for cost class

--- backend/calculations/cost_function.py
+++ backend/calculations/cost_function.py
@@ -1,5 +1,31 @@
-def main():
-    print('Here we will create the formulas to develop our cost function')
+from backend.data_structures.node import Node
 
-if __name__ == '__main__':
-    main()
+class cost:
+
+    def __init__(self, src: Node, dest: Node):
+        self.src = src
+        self.dest = dest
+        # Paramters for Boeing 737 model (uncomment)
+
+        # self.k = 3.16 #C)2/kg fuel burned
+        # self.fuel_mass_flow = #(kg/s)
+        # self.specific_fuel_consumption = #(kg of fuel/thrust/second)
+        # self.aircraft_weight = 
+        # self.LD = #Lift-to-drag ratio at cruise 
+
+    # Using the Haversine equation to calcuate the distance between two points
+    # Output: distance (in Km)
+    def get_distance(self):
+        pass
+
+    # first calcaute fuel mass, then calculate C02
+    def get_carbon_emissions(self):
+        pass 
+    
+    # Might use flight history for heatmap 
+    # traffic 
+    def get_air_traffic_desity(self):
+        pass 
+
+
+    
