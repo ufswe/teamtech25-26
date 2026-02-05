acc9b9c7bccedd7a85b5e5ba8fc6609e83347831
Olive <oliveodellpb@gmail.com>
2026-02-04 20:13:51 -0500

    fixed mohanas typos

--- backend/calculations/cost_function.py
+++ backend/calculations/cost_function.py
@@ -5,7 +5,7 @@ class cost:
     def __init__(self, src: Node, dest: Node):
         self.src = src
         self.dest = dest
-        # Paramters for Boeing 737 model (uncomment)
+        # Parameters for Boeing 737 model (uncomment)
 
         # self.k = 3.16 #C)2/kg fuel burned
         # self.fuel_mass_flow = #(kg/s)
@@ -13,18 +13,18 @@ class cost:
         # self.aircraft_weight = 
         # self.LD = #Lift-to-drag ratio at cruise 
 
-    # Using the Haversine equation to calcuate the distance between two points
+    # Using the Haversine equation to calculate the distance between two points
     # Output: distance (in Km)
     def get_distance(self):
         pass
 
-    # first calcaute fuel mass, then calculate C02
+    # first calculate fuel mass, then calculate C02
     def get_carbon_emissions(self):
         pass 
     
     # Might use flight history for heatmap 
     # traffic 
-    def get_air_traffic_desity(self):
+    def get_air_traffic_density(self):
         pass 
 
 
