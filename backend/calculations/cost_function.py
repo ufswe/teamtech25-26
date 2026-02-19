21fa9508e09b0355c4af4ed565e2944180f9422d
Mia Sanders <mia.sanders@ufl.edu>
2026-02-18 20:34:18 -0500

    Psuedo code and such

--- backend/calculations/cost_function.py
+++ backend/calculations/cost_function.py
@@ -1,12 +1,13 @@
 from turtle import distance
 from backend.data_structures.node import Node
 import math
+import numpy as np
 
 class cost:
 
     def __init__(self, src: Node, dest: Node):
-        self.src = src
-        self.dest = dest
+        self.src = src #in lat and long
+        self.dest = dest #in lat and long
         self.total_distance = 0
         self.dist_btw_layers = 50
         
@@ -37,11 +38,14 @@ class cost:
 
     # Using the Haversine equation to calculate the distance between two points
     # Output: distance (in Km)
-        
-    def get_distance(self, lat1, lon1, lat2, lon2):
-        import math
+
+    def lat_long_to_radians(self, lat1, lon1, lat2, lon2):
         # Convert latitude and longitude from degrees to radians (assume in decimal degrees)
         lat1, lon1, lat2, lon2 = map(math.radians, [lat1, lon1, lat2, lon2])
+
+        return lat1, lon1, lat2, lon2
+
+    def get_distance(self, lat1, lon1, lat2, lon2):
 
         # Haversine formula
         dlat = lat2 - lat1
@@ -54,6 +58,53 @@ class cost:
         distance = c*r
 
         return distance
+    
+    def lat_long_to_cartesian():
+    # pull lat_long
+
+    # do math & make new vector (cartesian_src_dest)
+    # return cartesian_src_dest
+
+
+    def cartesian_to_lat_long():
+	# pull cartesian
+
+	# do math & make new vector (lat_long_src_dest)
+    # return lat_long_src_dest
+
+
+    def get_nodes_per_layer():
+
+	# convert from spherical to cartesian, call spherical_to_cartesian()
+
+	# num_of_nodes=4
+
+	# dist_btw_nodes=5
+
+	# dist_btw_layer=50
+
+	# create a loop that will iterate from 0 to the number of layers-1
+
+		# calculate orthogonal vector
+		
+		# find magnitude of layer vectors (multiply 2 x dist_btw_nodes)
+		
+		# create a loop that will iterate from 0 to num_of_nodes-1
+
+			# add node on the line
+
+			# add 
+
+			# multiply the two calculated node values by -1
+
+			# add the four calculated node values for each layer to an array
+
+			# add the new array to a node network
+
+	
+
+	# return node network
+
 
     # first calculate fuel mass, then calculate C02
     def get_carbon_emissions(self):
