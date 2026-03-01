e58a029ef385df2bb62d40ee40c241fb05438183
savmaay <savanapham@ufl.edu>
2026-03-01 14:42:42 -0500

    shared components

---
+++ frontend/src/components/Card.jsx
@@ -0,0 +1,8 @@
+import "./Card.css"
+
+export default function Card({ title, children }){
+    return <div className="card">
+        {title && <h2 className="card-title">{title}</h2>}
+        {children}
+        </div>;
+}
