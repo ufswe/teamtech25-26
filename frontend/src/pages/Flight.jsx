267774ef39796c8391302b1d5ddd3d2ddd6bd6b6
Nane <nanekode@gmail.com>
2026-04-01 20:11:08 -0400

    works now

--- frontend/src/pages/Flight.jsx
+++ frontend/src/pages/Flight.jsx
@@ -1,7 +1,4 @@
 import { useState } from 'react';
-import { DayPicker } from 'react-day-picker';
-import { format } from 'date-fns';
-import "react-day-picker/dist/style.css";
 import "../components/Map.css";
 import "../styles/flight.css";
 import Map from "../components/Map";
