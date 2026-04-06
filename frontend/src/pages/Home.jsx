89a66907fac5dc0b97cb566de2e33fe93cb4e40e
savmaay <savanapham@ufl.edu>
2026-04-06 13:52:25 -0400

    final

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import { useNavigate } from "react-router-dom";
 import Button from "../components/Button.jsx";
 
 export default function Home() {
-  const navigate = useNavigate(); // REQUIRED
+  const navigate = useNavigate();
 
   return (
     <div
